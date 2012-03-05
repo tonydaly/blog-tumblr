fee50e27a0739b4b144f72846677811385cf578e
Tony Daly <tony.daly@me.com>
2012-03-05 14:09:20 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTumblr::Application.config.secret_token = '30327c2b4bf606b02ce20f746590724fda6ff83bf91dd9415abbc44c7d72fdf07562363c51373e9f7976b2cead72215bf5a0d4beaa4fe11456a71c40bd6a308b'
